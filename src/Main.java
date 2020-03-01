a727e84ef5a243d99f9c6a915caf189711603609
Alexey Suchkov <alexey.suchkov@gmail.com>
2020-03-01 18:46:04 +0300

    corrected

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,9 @@
-package PACKAGE_NAME;
-
 public class Main {
+    public static void main(String[] args) {
+        boolean registered = true;
+        int bonusCount = registered ? 20 : 0;
+        long pricePaid = 12575;
+        long bonus = pricePaid / bonusCount;
+        System.out.println(bonus);
+    }
 }
